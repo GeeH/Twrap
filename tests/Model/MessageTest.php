2b908066f485689d2c3d5135f621e95248146f37
Gary Hockin <gary@hock.in>
2021-03-31 10:04:23 +0100

    Removed serialization of arrays for readability, thanks @ocramius

--- tests/Model/MessageTest.php
+++ tests/Model/MessageTest.php
@@ -6,10 +6,54 @@ use Twrap\Model\Message;
 it(
     'creates a fully hydrated message with DateTimeImmutable dates from good data',
     function () {
-        $data =
-            unserialize(
-                'a:20:{s:4:"body";s:12:"Hello world!";s:11:"numSegments";s:1:"1";s:9:"direction";s:14:"outbound-reply";s:4:"from";s:13:"+447723427411";s:2:"to";s:13:"+447446188852";s:11:"dateUpdated";O:8:"DateTime":3:{s:4:"date";s:26:"2021-03-18 11:04:08.000000";s:13:"timezone_type";i:1;s:8:"timezone";s:6:"+00:00";}s:5:"price";s:8:"-0.04000";s:12:"errorMessage";N;s:3:"uri";s:104:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json";s:10:"accountSid";s:34:"ACb877821242bbaedc246328ca0a8c3fc6";s:8:"numMedia";s:1:"0";s:6:"status";s:9:"delivered";s:19:"messagingServiceSid";N;s:3:"sid";s:34:"SM0e3038708823b199692d85138d96e67c";s:8:"dateSent";O:8:"DateTime":3:{s:4:"date";s:26:"2021-03-18 11:04:06.000000";s:13:"timezone_type";i:1;s:8:"timezone";s:6:"+00:00";}s:11:"dateCreated";O:8:"DateTime":3:{s:4:"date";s:26:"2021-03-18 11:04:06.000000";s:13:"timezone_type";i:1;s:8:"timezone";s:6:"+00:00";}s:9:"errorCode";N;s:9:"priceUnit";s:3:"USD";s:10:"apiVersion";s:10:"2010-04-01";s:15:"subresourceUris";a:2:{s:5:"media";s:110:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json";s:8:"feedback";s:113:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json";}}'
-            );
+        $data = [
+            'body' => 'Hello world!',
+            'numSegments' => '1',
+            'direction' => 'outbound-reply',
+            'from' => '+447723427411',
+            'to' => '+447123456782',
+            'dateUpdated' =>
+                DateTime::__set_state(
+                    [
+                        'date' => '2021-03-18 11:04:08.000000',
+                        'timezone_type' => 1,
+                        'timezone' => '+00:00',
+                    ]
+                ),
+            'price' => '-0.04000',
+            'errorMessage' => null,
+            'uri' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json',
+            'accountSid' => 'ACb877821242bbaedc246328ca0a8c3fc6',
+            'numMedia' => '0',
+            'status' => 'delivered',
+            'messagingServiceSid' => null,
+            'sid' => 'SM0e3038708823b199692d85138d96e67c',
+            'dateSent' =>
+                DateTime::__set_state(
+                    [
+                        'date' => '2021-03-18 11:04:06.000000',
+                        'timezone_type' => 1,
+                        'timezone' => '+00:00',
+                    ]
+                ),
+            'dateCreated' =>
+                DateTime::__set_state(
+                    [
+                        'date' => '2021-03-18 11:04:06.000000',
+                        'timezone_type' => 1,
+                        'timezone' => '+00:00',
+                    ]
+                ),
+            'errorCode' => null,
+            'priceUnit' => 'USD',
+            'apiVersion' => '2010-04-01',
+            'subresourceUris' =>
+                [
+                    'media' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json',
+                    'feedback' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json',
+                ],
+        ];
+
         $message = Message::fromArray($data);
         $this->assertInstanceOf(DateTimeImmutable::class, $message->dateCreated);
         $this->assertInstanceOf(DateTimeImmutable::class, $message->dateSent);
@@ -20,9 +64,33 @@ it(
 it(
     'creates a hydrated message with null dates from bad date data',
     function () {
-        $data = unserialize(
-            'a:20:{s:4:"body";s:12:"Hello world!";s:11:"numSegments";s:1:"1";s:9:"direction";s:14:"outbound-reply";s:4:"from";s:13:"+447723427411";s:2:"to";s:13:"+447446188852";s:11:"dateUpdated";N;s:5:"price";s:8:"-0.04000";s:12:"errorMessage";N;s:3:"uri";s:104:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json";s:10:"accountSid";s:34:"ACb877821242bbaedc246328ca0a8c3fc6";s:8:"numMedia";s:1:"0";s:6:"status";s:9:"delivered";s:19:"messagingServiceSid";N;s:3:"sid";s:34:"SM0e3038708823b199692d85138d96e67c";s:8:"dateSent";N;s:11:"dateCreated";N;s:9:"errorCode";N;s:9:"priceUnit";s:3:"USD";s:10:"apiVersion";s:10:"2010-04-01";s:15:"subresourceUris";a:2:{s:5:"media";s:110:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json";s:8:"feedback";s:113:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json";}}'
-        );
+        $data = [
+            'body' => 'Hello world!',
+            'numSegments' => '1',
+            'direction' => 'outbound-reply',
+            'from' => '+447723427411',
+            'to' => '+447446188852',
+            'dateUpdated' => null,
+            'price' => '-0.04000',
+            'errorMessage' => null,
+            'uri' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json',
+            'accountSid' => 'ACb877821242bbaedc246328ca0a8c3fc6',
+            'numMedia' => '0',
+            'status' => 'delivered',
+            'messagingServiceSid' => null,
+            'sid' => 'SM0e3038708823b199692d85138d96e67c',
+            'dateSent' => null,
+            'dateCreated' => null,
+            'errorCode' => null,
+            'priceUnit' => 'USD',
+            'apiVersion' => '2010-04-01',
+            'subresourceUris' =>
+                [
+                    'media' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json',
+                    'feedback' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json',
+                ],
+        ];
+
         $message = Message::fromArray($data);
         $this->assertNull($message->dateCreated);
         $this->assertNull($message->dateSent);
@@ -32,20 +100,68 @@ it(
 
 it(
     'throws an exception when the to phone number can\'t be parsed',
-    function() {
-        $data = unserialize(
-            'a:20:{s:4:"body";s:12:"Hello world!";s:11:"numSegments";s:1:"1";s:9:"direction";s:14:"outbound-reply";s:4:"from";s:13:"+447723427411";s:2:"to";s:13:"+XXXXXXXXXXXX";s:11:"dateUpdated";N;s:5:"price";s:8:"-0.04000";s:12:"errorMessage";N;s:3:"uri";s:104:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json";s:10:"accountSid";s:34:"ACb877821242bbaedc246328ca0a8c3fc6";s:8:"numMedia";s:1:"0";s:6:"status";s:9:"delivered";s:19:"messagingServiceSid";N;s:3:"sid";s:34:"SM0e3038708823b199692d85138d96e67c";s:8:"dateSent";N;s:11:"dateCreated";N;s:9:"errorCode";N;s:9:"priceUnit";s:3:"USD";s:10:"apiVersion";s:10:"2010-04-01";s:15:"subresourceUris";a:2:{s:5:"media";s:110:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json";s:8:"feedback";s:113:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json";}}'
-        );
+    function () {
+        $data = [
+            'body' => 'Hello world!',
+            'numSegments' => '1',
+            'direction' => 'outbound-reply',
+            'from' => '+447723427411',
+            'to' => '+XXXXXXXXXXXX',
+            'dateUpdated' => null,
+            'price' => '-0.04000',
+            'errorMessage' => null,
+            'uri' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json',
+            'accountSid' => 'ACb877821242bbaedc246328ca0a8c3fc6',
+            'numMedia' => '0',
+            'status' => 'delivered',
+            'messagingServiceSid' => null,
+            'sid' => 'SM0e3038708823b199692d85138d96e67c',
+            'dateSent' => null,
+            'dateCreated' => null,
+            'errorCode' => null,
+            'priceUnit' => 'USD',
+            'apiVersion' => '2010-04-01',
+            'subresourceUris' =>
+                [
+                    'media' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json',
+                    'feedback' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json',
+                ],
+        ];
+
         Message::fromArray($data);
     }
 )->throws(PhoneNumberParseException::class);
 
 it(
     'throws an exception when the from phone number can\'t be parsed',
-    function() {
-        $data = unserialize(
-            'a:20:{s:4:"body";s:12:"Hello world!";s:11:"numSegments";s:1:"1";s:9:"direction";s:14:"outbound-reply";s:4:"from";s:13:"+XXXXXXXXXXXX";s:2:"to";s:13:"+447446188852";s:11:"dateUpdated";N;s:5:"price";s:8:"-0.04000";s:12:"errorMessage";N;s:3:"uri";s:104:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json";s:10:"accountSid";s:34:"ACb877821242bbaedc246328ca0a8c3fc6";s:8:"numMedia";s:1:"0";s:6:"status";s:9:"delivered";s:19:"messagingServiceSid";N;s:3:"sid";s:34:"SM0e3038708823b199692d85138d96e67c";s:8:"dateSent";N;s:11:"dateCreated";N;s:9:"errorCode";N;s:9:"priceUnit";s:3:"USD";s:10:"apiVersion";s:10:"2010-04-01";s:15:"subresourceUris";a:2:{s:5:"media";s:110:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json";s:8:"feedback";s:113:"/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json";}}'
-        );
+    function () {
+        $data = [
+            'body' => 'Hello world!',
+            'numSegments' => '1',
+            'direction' => 'outbound-reply',
+            'from' => '+XXXXXXXXXXXX',
+            'to' => '+447446188852',
+            'dateUpdated' => null,
+            'price' => '-0.04000',
+            'errorMessage' => null,
+            'uri' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c.json',
+            'accountSid' => 'ACb877821242bbaedc246328ca0a8c3fc6',
+            'numMedia' => '0',
+            'status' => 'delivered',
+            'messagingServiceSid' => null,
+            'sid' => 'SM0e3038708823b199692d85138d96e67c',
+            'dateSent' => null,
+            'dateCreated' => null,
+            'errorCode' => null,
+            'priceUnit' => 'USD',
+            'apiVersion' => '2010-04-01',
+            'subresourceUris' =>
+                [
+                    'media' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Media.json',
+                    'feedback' => '/2010-04-01/Accounts/ACb877821242bbaedc246328ca0a8c3fc6/Messages/SM0e3038708823b199692d85138d96e67c/Feedback.json',
+                ],
+        ];
+
         Message::fromArray($data);
     }
 )->throws(PhoneNumberParseException::class);
